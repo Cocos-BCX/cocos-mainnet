26d22c70a798c1b9762cae3ff31c0ceac590c60d
ypenghui6 <ypenghui6@163.com>
2020-03-15 14:41:08 +0800

    fix bug

--- libraries/app/application.cpp
+++ libraries/app/application.cpp
@@ -344,15 +344,20 @@ public:
           genesis_state_type genesis = fc::json::from_string(genesis_str).as<genesis_state_type>();
 
           //remove the extension   -----yp add -----
-          std::string pattern_without_extension = "extensions\": /[/]";
+          std::string pattern_without_extension = "extensions\": \[\]";
           boost::regex reg_without_extension(pattern_without_extension);
-          std::string pattern_with_extension = "extensions\": /[*/]";
+          std::string pattern_with_extension = "extensions\": \[*\]";
           boost::regex reg_with_extension(pattern_with_extension);
           boost::match_results<std::string::const_iterator> what;
-          if (!boost::regex_match(genesis_str, what, reg_without_extension) && boost::regex_match(genesis_str, reg_with_extension))
+          ilog("----------------------- ${str}", ("str", genesis_str));
+          if (!boost::regex_match(genesis_str, what, reg_without_extension)))
           {
-            boost::regex_replace(genesis_str, reg_without_extension, pattern_without_extension);
-            ilog("----------------------- ${str}", ("str", genesis_str));
+            ilog("+++++++++++++++++++++++++ ${str}", ("str", genesis_str));
+            if (boost::regex_match(genesis_str, reg_with_extension))
+            {
+              boost::regex_replace(genesis_str, reg_without_extension, "extensions\": []");
+              ilog("........................... ${str}", ("str", genesis_str));
+            }
           }
 
           //idump((genesis.initial_parameters.maximum_run_time_ratio));
