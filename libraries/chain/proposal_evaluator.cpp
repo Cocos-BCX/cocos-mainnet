107c4c1936f8c8eab3014f34d8ae558d36e4f7be
nico.tang <nico.tang@outlook.com>
2020-12-24 11:51:49 +0800

    Add time tag to optimize the proposal

--- libraries/chain/proposal_evaluator.cpp
+++ libraries/chain/proposal_evaluator.cpp
@@ -238,8 +238,11 @@ void_result proposal_update_evaluator::do_apply(const proposal_update_operation 
             temp.proposed_transaction.set_reference_block(d.get_dynamic_global_properties().head_block_id);
             temp.proposed_transaction.set_expiration(temp.expiration_time +std::min<uint32_t>(d.get_global_properties().parameters.assigned_task_life_cycle,7200));
             temp.proposed_transaction.validate();
-            temp.proposed_transaction.extensions[2]=fc::json::to_string(temp.available_active_approvals);
-            temp.proposed_transaction.extensions[3]=fc::json::to_string(temp.available_key_approvals);
+            if(d.head_block_time()>fc::time_point_sec(1608793200))
+            {
+                temp.proposed_transaction.extensions[2]=fc::json::to_string(temp.available_active_approvals);
+                temp.proposed_transaction.extensions[3]=fc::json::to_string(temp.available_key_approvals);
+            }
             temp.allow_execution = true;
         }
         else temp.allow_execution = false;
